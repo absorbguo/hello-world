8fa4789f9b875a01da250879dd9b64991fe9e33d
absorb guo <1173982045@qq.com>
2016-08-12 22:54:40 +0800

    test.py is modified at local

--- test.py
+++ test.py
@@ -2,5 +2,6 @@ import sys
 def main():
   print "this file is contained in the devel branch"
   print "test"
+  print "this test is maintained in master branch"
 if __name__ == "__main__":
   main()
